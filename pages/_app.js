d106a65c8cba705d3317c899a18cd680ae933660
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-05-08 08:32:17 +0300

    File-Based Routing practice is done

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { useEffect, useState } from "react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
     // const [showChild, setShowChild] = useState(false);
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,11 @@
-/*
- *** NextJS pre-rendering page automatically dy default, but if use getStaticPros(context){} 
- which tells to NextJS will not pre rendered a page, it's kind of confirm to NextJS to JS that
-  this page still should be pre-generated. It's run on the server, 
-  not in the browser and during the build process, after it was deployed
- */
-function HomePage(props) {
-    const { products } = props;
+function HomePage() {
+
     return (
         <>
             <h2>This is home page</h2>
-
-            <ul>
-                {products.map((product) => (
-                    <li key={product.id}>{product.title}</li>
-                ))}
-            </ul>
         </>
     );
 }
 
-export async function getStaticProps() {
-    return {
-        props: {
-            products: [
-                {
-                    id: "p1",
-                    title: "Product 1",
-                },
-            ],
-        },
-    };
-}
 
 export default HomePage;
