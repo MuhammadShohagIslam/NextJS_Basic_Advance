a3dd469956182db2cd37f8de3299f2e007bf18b2
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-05-08 17:59:26 +0300

    getStaticProps and getStaticProps practice is done

--- pages/ppre-rendering/[productId].js
+++ pages/ppre-rendering/[productId].js
@@ -1,35 +1,51 @@
+import fs from "fs/promises";
+import path from "path";
 import { Fragment } from "react";
-import path from 'path';
-import fs from 'fs/promises'
 
-/*
-    *** getStaticProps(context) --- context param ***
-        --- to use context param, to get hold of the concrete param values, so the concrete values for those dynamic segments of our path.
-*/
-
-const ProductDetailsPage = ({product}) => {
+const ProductDetailsPage = ({ product }) => {
+    //this condition check for reson of  fallback: true
+    if (!product) {
+        return <p>Loading...</p>;
+    }
     return (
         <Fragment>
             <h1>{product.title}</h1>
             <p>{product.description}</p>
         </Fragment>
     );
-}
+};
 
-export async function getStaticProps(context){
+async function readFileFunction() {
     const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
     const jsonData = await fs.readFile(filePath);
     const data = JSON.parse(jsonData);
+    return {
+        data,
+    };
+}
+
+/*
+    *** getStaticProps(context) --- context param ***
+        --- to use context param, to get hold of the concrete param values, so the concrete values for those dynamic segments of our path.
+*/
+export async function getStaticProps(context) {
+    const { data } = await readFileFunction();
 
     const { params } = context;
     const productId = params.productId;
-    const singleProduct = data.products.find(product => product.id === productId);
-
-    return{
-        props:{
-            product: singleProduct
-        }
+    const singleProduct = data.products.find(
+        (product) => product.id === productId
+    );
+    if (!singleProduct) {
+        return {
+            notFound: true,
+        };
     }
+    return {
+        props: {
+            product: singleProduct,
+        },
+    };
 }
 
 /*
@@ -39,17 +55,30 @@ export async function getStaticProps(context){
 
     * If we have dynamic page like [productId], default behaviour is not pre-generate the page beacuse NextJs does not know how many page re-rendering, concrete values of this dynamic segment we need.
 
+    *** fallback can help you if we have a lot of pages that would need to be pre-generated.
+            --- if we set fallback: true (We can pre-generated some pages what we want) or even if an productId value is not found here, we still might be able to render a page, so we should use if condition 
+            --- if we set fallback: "blocking" (we do not need cheack condition), it's take longer, if we do not show uncompleted page
+
 
 */
-export async function getStaticPaths(){
-    return{
+export async function getStaticPaths() {
+    const { data } = await readFileFunction();
+    const dynamicParams = data.products.map((product) => ({
+        params: { productId: product.id },
+    }));
+    return {
+        // dynamic way parmas set
+        paths: dynamicParams,
+        /*
         paths: [
             {params: { productId: "p1" }},
-            {params: { productId: "p2" }},
-            {params: { productId: "p3" }}
+            // {params: { productId: "p2" }}, // if we do not want to pre-generated higly visited page
+            // {params: { productId: "p3" }}
         ],
-        fallback: false
-    }
+        fallback: "blocking"
+    */
+        fallback: true,
+    };
 }
 
 export default ProductDetailsPage;
